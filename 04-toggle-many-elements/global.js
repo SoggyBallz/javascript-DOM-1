3be34408604fcaa968581fbb5b182a3966e247ca
SoggyBallz <girtsjans@gmail.com>
2021-03-23 15:35:36 +0200

    gey

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,14 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+    document.getElementById("button").addEventListener("click",function(){
+        var els = document.getElementsByClassName("toggle_me");
+        for(var i=0; i < els.length; i++){
+            if(els[i].style.display === "none"){
+                els[i].style.display = "block"
+            }
+            else{
+                els[i].style.display = "none"
+            }
+
+        }
+    });
+});
